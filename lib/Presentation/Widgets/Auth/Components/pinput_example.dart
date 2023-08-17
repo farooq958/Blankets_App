40a1f85435f911558d4d99e3c21839fb5ec226c3
Muhammad Haris Akhtar <muhammadharis9603@gmail.com>
2023-08-17 17:13:23 +0500

    17 august 2

--- lib/Presentation/Widgets/Auth/Components/pinput_example.dart
+++ lib/Presentation/Widgets/Auth/Components/pinput_example.dart
@@ -68,6 +68,7 @@ class _PinputExampleState extends State<PinputExample> {
               AndroidSmsAutofillMethod.smsUserConsentApi,
               listenForMultipleSmsOnAndroid: true,
               defaultPinTheme: defaultPinTheme,
+
               separatorBuilder: (index) => const SizedBox(width: 8),
               validator: (value) {
                 return value == '2222' ? null : 'Pin is incorrect';
@@ -94,19 +95,8 @@ class _PinputExampleState extends State<PinputExample> {
                   ),
                 ],
               ),
-              focusedPinTheme: defaultPinTheme.copyWith(
-                decoration: defaultPinTheme.decoration!.copyWith(
-                  borderRadius: BorderRadius.circular(8),
-                  border: Border.all(color: focusedBorderColor),
-                ),
-              ),
-              submittedPinTheme: defaultPinTheme.copyWith(
-                decoration: defaultPinTheme.decoration!.copyWith(
-                  color: fillColor,
-                  borderRadius: BorderRadius.circular(19),
-                  border: Border.all(color: focusedBorderColor),
-                ),
-              ),
+              focusedPinTheme: defaultPinTheme,
+              submittedPinTheme: defaultPinTheme,
               errorPinTheme: defaultPinTheme.copyBorderWith(
                 border: Border.all(color: Colors.redAccent),
               ),

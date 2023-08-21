4a7da5fe8dc9e4ad5cde21e3b097392d09db5ab0
Muhammad Haris Akhtar <muhammadharis9603@gmail.com>
2023-08-21 15:31:30 +0500

    21 august fixes

--- lib/Presentation/Common/expandable_section.dart
+++ lib/Presentation/Common/expandable_section.dart
@@ -30,67 +30,82 @@ class _ExpandableState extends State<ExpandableSection> {
           isExpanded = !isExpanded;
         });
       },
-      child: Material(
-        borderRadius: BorderRadius.circular(10.r),
-        elevation: 4.sp,
-        child: AnimatedContainer(
-          duration: const Duration(milliseconds: 300),
-          padding: EdgeInsets.symmetric(horizontal: 10.w),
-          width: 1.sw,
-          // height: !isExpanded ? 50.h:null,
-          decoration: BoxDecoration(
-            color: AppColors.whiteColor,
-            borderRadius: BorderRadius.circular(10.r),
-          ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 300),
+        // padding: EdgeInsets.symmetric(vertical: 5.h),
+        width: 1.sw,
+       // margin: EdgeInsets.symmetric(horizontal: 20),
+        // height: !isExpanded ? 50.h:null,
+        decoration: BoxDecoration(
+          color: isExpanded? const Color(0xffE7EDF5) : AppColors.whiteColor,
+          borderRadius: BorderRadius.circular(10.r),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0x0F000000),
+              blurRadius: 12,
+              offset: Offset(0, 6),
+              spreadRadius: 0,
+            )
 
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
-                      children: [
-                       widget.isImageRequired != null? AssetImageWidget(
-                          url: Assets.appLogo,
-                          color: AppColors.primaryColor,
-                          width: 50.w,
-                          height: 50.h,
-                        ) : const SizedBox(),
-                        CustomSizedBox.width(5.w),
-                        SizedBox(
-                          width: 230.w,
-                          child: AppText(widget.title,
-                              style: Styles.circularStdBold(context),maxLine: 2,),
-                        ),
-                      ],
+          ]
+        ),
+        child: Padding(
+          padding:  EdgeInsets.only(top: 5.h),
+          child: Column(
+
+
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                children: [
+
+                  Expanded(
+                    child: Container(
+                      margin:  widget.isImageRequired !=  null ? EdgeInsets.only(left: 5.sp):EdgeInsets.only(left: 20.sp),
+                      child: Row(
+                        children: [
+                             widget.isImageRequired != null? AssetImageWidget(
+                                url: Assets.appLogo,
+                                color: AppColors.primaryColor,
+                                width: 50.w,
+                                height: 50.h,
+                              ) : const SizedBox(),
+                           Expanded(
+                             child: Align(
+                              child: AppText(widget.title,
+
+                                style: isExpanded ? Styles.circularStdBold(context,fontSize: 15.sp) : Styles.circularStdRegular(context,fontSize: 16.sp),maxLine: 3,),
+
+                          ),
+                           ),
+                        ],
+                      ),
                     ),
-                    IconButton(
-                        onPressed: () {
-                          // Future.delayed(Duration(seconds: 2));
-                          setState(() {
-                            isExpanded = !isExpanded;
-                          });
-                        },
-                        icon: Icon(
-                          isExpanded
-                              ? Icons.keyboard_arrow_up
-                              : Icons.keyboard_arrow_down,
-                          color: AppColors.primaryColor,
-                        )),
-                  ],
-                ),
-                CustomSizedBox.height(10.h),
-                if (isExpanded) ...[
-                  // Additional content when expanded
-                  // You can add more widgets here
-                  widget.expendedContent,
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        // Future.delayed(Duration(seconds: 2));
+                        setState(() {
+                          isExpanded = !isExpanded;
+                        });
+                      },
+                      icon: Icon(
+                        isExpanded
+                            ? Icons.keyboard_arrow_up
+                            : Icons.keyboard_arrow_down,
+                        color: AppColors.primaryColor,
+                      )),
                 ],
+              ),
+              CustomSizedBox.height(10.h),
+              if (isExpanded) ...[
+                // Additional content when expanded
+                // You can add more widgets here
+                widget.expendedContent,
               ],
-            ),
+            ],
           ),
         ),
       ),
--- lib/Presentation/Widgets/FAQS/faqs_screen.dart
+++ lib/Presentation/Widgets/FAQS/faqs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:hbk/Data/DataSource/Resources/Extensions/extensions.dart';
 import 'package:hbk/Data/DataSource/Resources/imports.dart';
 
 class FAQS extends StatelessWidget {
@@ -14,64 +15,73 @@ class FAQS extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             CustomSizedBox.height(20.h),
-            ExpandableTileWidget(
-                isPaddingRequired: true,
-                text: 'How do I place an order for a blanket on the app?',
-                expendedContent: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 35.w),
-                      child: AppText(
-                        'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
-                        style:
-                            Styles.circularStdMedium(context, fontSize: 16.sp),
-                        maxLine: 6,
-                        textAlign: TextAlign.justify,
-                      ),
-                    )
-                  ],
-                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: ExpandableSection(
+                //isImageRequired: false,
+                  title: 'How do I place an order for a blanket on the app?', expendedContent: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 18.w),
+                    child: AppText(
+                      'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
+                      style:
+                      Styles.circularStdMedium(context, fontSize: 16.sp),
+                      maxLine: 5,
+                      textAlign: TextAlign.justify,
+                    ),
+                  )
+                ],
+              )),
+            ),
             CustomSizedBox.height(10.h),
-            ExpandableTileWidget(
-                isPaddingRequired: true,
-                text:
-                    'Is there a loyalty rewards program for frequent customers?',
-                expendedContent: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 35.w),
-                      child: AppText(
-                        'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
-                        style:
-                            Styles.circularStdMedium(context, fontSize: 16.sp),
-                        maxLine: 5,
-                        textAlign: TextAlign.justify,
-                      ),
-                    )
-                  ],
-                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: ExpandableSection(
+                //isImageRequired: false,
+                  title: 'Is there a loyalty rewards program for frequent customers?', expendedContent: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 18.w),
+                    child: AppText(
+                      'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
+                      style:
+                      Styles.circularStdMedium(context, fontSize: 16.sp),
+                      maxLine: 5,
+                      textAlign: TextAlign.justify,
+                    ),
+                  )
+                ],
+              )),
+            ),
             CustomSizedBox.height(10.h),
-            ExpandableTileWidget(
-                isPaddingRequired: true,
-                text: 'How can I contact customer support if I have a question',
-                expendedContent: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 35.w),
-                      child: AppText(
-                        'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
-                        style:
-                            Styles.circularStdMedium(context, fontSize: 16.sp),
-                        maxLine: 5,
-                        textAlign: TextAlign.justify,
-                      ),
-                    )
-                  ],
-                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: ExpandableSection(
+                //isImageRequired: false,
+                  title: 'How can I contact customer support if I have a question', expendedContent: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 18.w),
+                    child: AppText(
+                      'To place an order, simply browse through our blanket collection, select the one you like, and click on the "Add to Cart" button. Then, proceed to place order.',
+                      style:
+                      Styles.circularStdMedium(context, fontSize: 16.sp),
+                      maxLine: 5,
+                      textAlign: TextAlign.justify,
+                    ),
+                  )
+                ],
+              )),
+            ),
+
+            20.y
+            ,
+
+
           ],
         ),
       ),

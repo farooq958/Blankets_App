7e6c00205d8d8adb5a14f56e757cc46518dc3a00
Muhammad Haris Akhtar <muhammadharis9603@gmail.com>
2023-08-21 14:41:12 +0500

    21 august

--- lib/Presentation/Common/expandable_tile.dart
+++ lib/Presentation/Common/expandable_tile.dart
@@ -7,15 +7,21 @@ class ExpandableTileWidget extends StatefulWidget {
   final bool? leadingSvgIcon;
   final String? imageUrl;
   final bool? isPaddingRequired;
+  final ValueChanged<bool>? onExpansionChanged;
+  final ExpansionTileController? expansionTileController;
+  final bool? isExpanded;
 
-  const ExpandableTileWidget(
+   const ExpandableTileWidget(
       {Key? key,
-      this.text,
-      this.isImageRequired,
-      this.imageUrl,
-      required this.expendedContent,
-      this.isPaddingRequired,
-      this.leadingSvgIcon})
+        this.text,
+        this.isImageRequired,
+        this.imageUrl,
+        required this.expendedContent,
+        this.isPaddingRequired,
+        this.onExpansionChanged,
+        this.isExpanded,
+        this.expansionTileController,
+        this.leadingSvgIcon})
       : super(key: key);
 
   @override
@@ -23,69 +29,66 @@ class ExpandableTileWidget extends StatefulWidget {
 }
 
 class _ExpandableTileWidgetState extends State<ExpandableTileWidget> {
-  bool isExpanded = false;
+  // bool isExpanded = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Color(0x0F000000),
-            blurRadius: 12,
-            offset: Offset(0, 6),
-            spreadRadius: 0,
-          )
-        ]
+          boxShadow: [
+            BoxShadow(
+              color: Color(0x0F000000),
+              blurRadius: 12,
+              offset: Offset(0, 6),
+              spreadRadius: 0,
+            )
+          ]
       ),
       child: ExpansionTile(
 
         expandedAlignment: Alignment.center,
 // maintainState: true,
-        title: Text(
-      widget.text ?? 'Head Office',
-      style: Styles.circularStdBold(context, fontSize: 16.sp),
+        title: AppText(
+          widget.text ?? 'Head Office',
+          style: Styles.circularStdBold(context, fontSize: 16.sp),
+          maxLine: 2,
         ),
         iconColor: AppColors.primaryColor,
         collapsedIconColor: AppColors.primaryColor,
         leading: widget.isImageRequired != null
-        ? widget.leadingSvgIcon != null
+            ? widget.leadingSvgIcon != null
             ? SvgPicture.asset(
-                widget.imageUrl!,
-                color: AppColors.primaryColor,
-              )
+          widget.imageUrl!,
+          color: AppColors.primaryColor,
+        )
             : Padding(
-                padding:  EdgeInsets.only(right: 10.0.sp),
-                child: AssetImageWidget(
-                  url: widget.imageUrl!,
-                  color: AppColors.primaryColor,
-                  width: 50.w,
-                  height: 100.h,
-                ),
-              )
-        : const SizedBox(),
+          padding:  EdgeInsets.only(right: 10.0.sp),
+          child: AssetImageWidget(
+            url: widget.imageUrl!,
+            color: AppColors.primaryColor,
+            width: 50.w,
+            height: 100.h,
+          ),
+        )
+            : const SizedBox(),
         tilePadding: widget.isPaddingRequired != null
-        ? EdgeInsets.only(right: 15.w)
-        : null,
-        onExpansionChanged: (value) {
-      setState(() {
-        isExpanded = !isExpanded;
-      });
-        },
+            ? EdgeInsets.only(right: 15.w)
+            : null,
+        onExpansionChanged: widget.onExpansionChanged,
         backgroundColor:const Color(0xffE7EDF5),
         collapsedBackgroundColor:AppColors.whiteColor ,
         shape: RoundedRectangleBorder(
-      borderRadius: BorderRadius.circular(10.r),
-        side: const BorderSide(color: Colors.white)
+            borderRadius: BorderRadius.circular(10.r),
+            side: const BorderSide(color: Colors.white)
         ),
         collapsedShape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(11.r),
-        side: const BorderSide(color: Colors.white)
+            borderRadius: BorderRadius.circular(11.r),
+            side: const BorderSide(color: Colors.white)
         ),
         children: [
-      if (isExpanded) ...[
-        widget.expendedContent,
-      ]
+          // if (widget.isExpanded!) ...[
+            widget.expendedContent,
+          // ]
         ],
       ),
     );
